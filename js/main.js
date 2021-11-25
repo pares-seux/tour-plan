31aa572161e25ba4246285a0b5de8cc261fafbe6
pares-seux <paresseux.992@gmail.com>
2021-11-25 14:16:18 +0400

    Fix: returned google map

--- js/main.js
+++ js/main.js
@@ -12,23 +12,4 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
-var myMap;
-ymaps.ready(init);
 
-function init () {
-  myMap = new ymaps.Map('map', {
-    center: [7.8907, 98.2946],
-    zoom: 17
-  }, {
-    searchControlProvider: 'yandex#search'
-  });
-
-var myGeoObject = new ymaps.GeoObject ({
-    geometry: {
-      type: "Point",
-      coordinates: [7.890759, 98.294690]
-    }
-  });
-
- myMap.GeoObject.add(myGeoObject); 
-};
