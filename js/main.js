4aceb0cf369d17487c068d00d1cfedbb2adf7a20
pares-seux <paresseux.992@gmail.com>
2021-11-29 14:30:15 +0400

    Create: added parallax

--- js/main.js
+++ js/main.js
@@ -27,4 +27,16 @@ const reviewSlider = new Swiper('.reviews-slider', {
   },
 });
 
+const newsLetter = document.getElementsByClassName('newsletter__parallax')[0];
+var showsHeight = document.body.scrollHeight - window.innerHeight;
+  
+
+
+function moveBg(e) {
+  var position = (window.pageYOffset/(showsHeight))*70;
+  newsLetter.style.backgroundPositionY = position + '%';
+};
+
+document.addEventListener('scroll', moveBg);
+
 
